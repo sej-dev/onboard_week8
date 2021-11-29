c0103226c0ad53f2660188f9bc14bd1be76b1ece
sej-dev <wndmstla1@gmail.com>
2021-11-29 13:35:51 +0900

    Feat: 상수 정의

---
+++ src/constants/operator.js
@@ -0,0 +1,8 @@
+const PLUS = 'PLUS';
+const MINUS = 'MINUS';
+const MULTIPLY = 'MULTIPLY';
+const DIVISION = 'DIVISION';
+
+export default {
+    PLUS, MINUS, MULTIPLY, DIVISION
+}
---
+++ src/constants/valueType.js
@@ -0,0 +1,16 @@
+const INTEGER = 'INTEGER';              // 정수
+const REAL_NUMBER = 'REAL_NUMBER';            // 실수
+const OPERATOR = 'OPERATOR';            // 연산자
+const REMOVE_ALL = 'REMOVE_ALL';        // 전체 삭제
+const REMOVE_RECENT = 'REMOVE_RECENT';  // 최근 삭제
+const REMOVE_ONE = 'REMOVE_ONE';        // 1글자 삭제
+const EQUAL = 'EQUAL';                  // =
+const DOT = 'DOT';                      // .
+const NEGATE = 'NEGATE';                // 부호 변환
+
+export default {
+    INTEGER, REAL_NUMBER,
+    OPERATOR, EQUAL,
+    REMOVE_ALL, REMOVE_RECENT, REMOVE_ONE,
+    DOT, NEGATE
+};
